cdc7779cfc13d269d646348c12a3c7c5084f3ba5
evheniys <evheniys@gmail.com>
2015-03-05 20:37:31 +0200

    05-03-01

--- app/Reservation.php
+++ app/Reservation.php
@@ -4,7 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model {
 
-	protected $table = 'customers';
+    protected $table = 'reservations';
+
+    protected $fillable = [ 'reservation_date',
+                            'reservation_time_from',
+                            'reservation_time_till',
+                            'comment'];
+
 
 
 }
